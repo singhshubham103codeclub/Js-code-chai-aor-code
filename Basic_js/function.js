3ac94eb2ade89d055b88beb890cf87fa68cd444c
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2024-09-24 20:18:49 +0000

    container config Done

--- Basic_js/function.js
+++ Basic_js/function.js
@@ -2,4 +2,5 @@ const Array=[100,200,300,]
 function returnArray(getArray){
     return getArray[2] 
 }
- console.log(returnArray(Array))
+//  console.log(returnArray(Array))
+ console.log(returnArray(100,200,300,]))

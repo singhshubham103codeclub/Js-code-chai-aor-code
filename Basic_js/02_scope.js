6848b87e6d396059d3e681bfeccba0d50f2ded6e
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2024-09-25 20:04:54 +0000

    container done

---
+++ Basic_js/02_scope.js
@@ -0,0 +1,20 @@
+let a=100
+{let a=20
+    // console.log(a)
+const b=10
+var c=30}
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(a)
+function one(){
+    const username="hitesh"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    console.log(website)
+    two()
+}
+one()
+
